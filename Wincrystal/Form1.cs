ec6d5bef4c0a2b885e91f3f7ee74a90b5ad3ad0f
rabbani <shaikrabbani241@outlook.com>
2017-07-03 12:37:20 +0530

    testcommint

---
+++ Wincrystal/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CrystalDecisions.CrystalReports.Engine;
+using System.Data.SqlClient;
+
+namespace Wincrystal
+{
+    public partial class Form1 : Form
+    {
+        ReportDocument rd = new ReportDocument();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            SqlConnection cn = new SqlConnection("Data Source=.;Initial Catalog=atmecs;Integrated Security=True");
+            SqlDataAdapter dr = new SqlDataAdapter("select*from products", cn);
+            DataSet st = new DataSet();
+            dr.Fill(st,"products");
+            rd.Load(@"C:\Users\shaik.rabbani\Desktop\Wincrystal\Wincrystal\CrystalReport.rpt");
+            rd.SetDataSource(st);
+            crystalReportViewer1.ReportSource = rd;
+        }
+    }
+}
